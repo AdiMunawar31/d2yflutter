6d5ea0b09fff4bdb73360c9dcd113d2bc8ccc47a
Adi Munawar <munawar.adi31@gmail.com>
2022-11-24 21:54:30 +0700

    add dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets/data/menu.dart';
 import 'package:flutter_widgets/models/menu_model.dart';
-import 'package:flutter_widgets/screens/dashboard.dart';
-import 'package:flutter_widgets/screens/home.dart';
-import 'package:flutter_widgets/screens/testing.dart';
+import 'package:flutter_widgets/widgets/drawer.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Menu> data = [];
+  bool isSwitched = false;
+
+  @override
+  void initState() {
+    for (var element in dataList) {
+      data.add(Menu.fromJson(element));
+    }
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter D2Y',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
-      ),
-      home: const MyHomePage(title: 'D2Y - Flutter'),
+      theme: isSwitched ? ThemeData.dark() : ThemeData.light(),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('D2Y - FLUTTER'),
+            actions: [
+              Switch(
+                  activeColor: Colors.greenAccent,
+                  value: isSwitched,
+                  onChanged: (value) {
+                    setState(() {
+                      isSwitched = !isSwitched;
+                    });
+                  }),
+            ],
+          ),
+          drawer: Drawer(
+            child: buildDrawer(data),
+          ),
+          body: const Center(
+            child: Text(
+              "Main Screen Contents...",
+              style: TextStyle(fontSize: 20),
+            ),
+          )),
     );
   }
 }
